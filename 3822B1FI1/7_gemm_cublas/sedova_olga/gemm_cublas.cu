f961d2fc704e1ec8e18bfd455ddbd687a8a05c4d
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-06-04 16:41:53 +0300

    test

--- 3822B1FI1/7_gemm_cublas/sedova_olga/gemm_cublas.cu
+++ 3822B1FI1/7_gemm_cublas/sedova_olga/gemm_cublas.cu
@@ -1,22 +1,16 @@
 #include "gemm_cublas.h"
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
-#include <vector>
-#include <stdexcept>
-#include <iostream>
 
 std::vector<float> GemmCUBLAS(const std::vector<float>& a,
                               const std::vector<float>& b,
                               int n) {
     if (n == 0) return {};
-
-    cublasHandle_t handle;
-    cublasCreate(&handle);
-
-    int size = n * n * sizeof(float);
     std::vector<float> c(n * n);
+    int size = n * n * sizeof(float);
 
-    float *d_a = nullptr, *d_b = nullptr, *d_c = nullptr, *d_ct = nullptr;
+    float* d_a, d_b, d_c;
+    float *d_ct;
 
     cudaMalloc(&d_a, size);
     cudaMalloc(&d_b, size);
@@ -26,11 +20,13 @@ std::vector<float> GemmCUBLAS(const std::vector<float>& a,
     cublasSetMatrix(n, n, sizeof(float), a.data(), n, d_a, n);
     cublasSetMatrix(n, n, sizeof(float), b.data(), n, d_b, n);
 
+    cublasHandle_t handle;
+    cublasCreate(&handle);
     const float alpha = 1.0f;
     const float beta = 0.0f;
 
     cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, n, n, n, &alpha, d_a, n, d_b, n, &beta, d_c, n);
-    cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, n, n, n, &alpha, d_c, n, d_b, n, &beta, d_ct, n);
+    cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, n, n, &alpha, d_c, n, &beta,  nullptr, n, d_ct, n);
 
     cublasGetMatrix(n, n, sizeof(float), d_ct, n, c.data(), n);
 
